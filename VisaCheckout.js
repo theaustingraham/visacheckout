de0c4ac1ef34df4c7e21e7cb55e384e3c4c22dde
algraham <austin.l.graham@lowes.com>
2016-08-19 12:20:53 -0400

    Guest User Functionality

--- VisaCheckout.js
+++ VisaCheckout.js
@@ -34,10 +34,14 @@ function onVisaCheckoutReady() {
 		
 		init: function(){
 			// -----
-			Lowes.Checkout.Visa.setupHtml();
+			if(Lowes.User.isSignedIn()){
+				Lowes.Checkout.Visa.setupUserHtml();
+			}else{
+				Lowes.Checkout.Visa.setupGuestHtml();
+			}
 		},
 
-		setupHtml: function(){
+		setupUserHtml: function(){
 			// ----- Inject the HTML into the payment page
 			var htmlContent;
 			// Add the initial tab
@@ -53,10 +57,23 @@ function onVisaCheckoutReady() {
 			$('.tab_content').append(htmlContent);
 
 			// Setup bindings
-			Lowes.Checkout.Visa.setupBindings();
+			Lowes.Checkout.Visa.setupUserBindings();
 		},
 
-		setupBindings: function(){
+		setupGuestHtml: function() {
+			// --- 
+			// Guest HTML For drop down...
+			$('option[value="VISA"]').after('<option value="VISACheckout">VISA Checkout</option>');
+			// Create container and move CC fields to it to make showing and hiding easier...
+			$('#card-exp-date').after('<div id="cc-Fields"></div>');
+			$('#card-number, #card-code, #card-exp-date').appendTo('#cc-Fields');
+			// Adding Visa Checkout Div and content
+			$('#cc-Fields').after('<div id="visa-co" class="display-none grid-100" >Visa Checkout provides a single sign-in service to pay for online shopping purchases. After a simple setup, Visa Checkout users can skip inputting their payment and shipping information for their orders. Whether at home or on the go, Visa Checkout works across multiple devices so online shopping stays easy. Visa Checkout enables shoppers to choose their preferred payment method across multiple retailers with a click of a button.<br><br><strong>Use the button below to checkout using visa checkout.</strong><br><br><img alt="Visa Checkout" class="v-button" style="width:40%;" role="button" src="https://sandbox.secure.checkout.visa.com/wallet-services-web/xo/button.png" /><br><br><a role="button" class="v-button js-edit-cc-card display-none" href="#">Click Here to Edit Your Card</a></div>');
+			
+			Lowes.Checkout.Visa.setupGuestBindings();
+		},
+
+		setupUserBindings: function(){
 			// -----
 			// Cached Selectors
 
@@ -100,6 +117,25 @@ function onVisaCheckoutReady() {
 
 		},
 
+		setupGuestBindings: function() {
+			$('#checkout-card-type').on('change', function(event) {
+				event.preventDefault();
+				/* Act on the event */
+				if(this.value == "VISACheckout"){
+					$('#cc-Fields').addClass('display-none');
+					$('#visa-co').removeClass('display-none');
+
+				}else{
+					if($('#visa-co').is(':visible')){
+						$('#cc-Fields').removeClass('display-none');
+						$('#visa-co').addClass('display-none');
+					}
+				}
+			});
+			// Finally - load in the Visa checkout scripts...
+			Lowes.Checkout.Visa.loadInVisaCheckout();
+		},
+
 		addHiddenFields: function(encKey, encPaymentData){
 			// -----
 			// Adds hidden form fields and their values....
@@ -114,9 +150,15 @@ function onVisaCheckoutReady() {
 			var htmlContent;
 
 			htmlContent = 'The remaining balance of <strong>'+ $('#remaining-balance').html() + '</strong> will be applied to your visa card.<br><br>';
-			htmlContent = htmlContent + '<img src="https://www.underconsideration.com/brandnew/archives/visa_2014_logo_detail.png" style="width:35px; padding-right:10px;" />x1242<br><strong>Expires:</strong> 11/21';
+			htmlContent = htmlContent + '<img src="https://www.underconsideration.com/brandnew/archives/visa_2014_logo_detail.png" style="width:35px; padding-right:10px;" />x1242<br><strong>Expires:</strong> 11/21<br><br>';
 
-			$('.js-visaInfo').html(htmlContent);
+			if(Lowes.User.isSignedIn()){
+				$('.js-visaInfo').html(htmlContent);
+			}else{
+				editCardbutton = $('.js-edit-cc-card').detach();
+				$('#visa-co').html(htmlContent);
+				editCardbutton.appendTo('#visa-co');
+			}
 			// Show button to re-activate the Visa Modal
 			$('.js-edit-cc-card').removeClass('display-none');
 
